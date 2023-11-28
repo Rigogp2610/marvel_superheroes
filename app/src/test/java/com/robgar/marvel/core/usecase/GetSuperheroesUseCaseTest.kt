768bc86cb63b879ebd0f8191f73b562c9675ec3a
Rigogp2610 <rgarcia2610p@gmail.com>
2023-11-28 23:55:47 +0100

    Add unit test

---
+++ app/src/test/java/com/robgar/marvel/core/usecase/GetSuperheroesUseCaseTest.kt
@@ -0,0 +1,50 @@
+package com.robgar.marvel.core.usecase
+
+import com.robgar.marvel.core.data.MarvelRepository
+import com.robgar.marvel.core.data.network.model.Superhero
+import com.robgar.marvel.core.data.network.model.SuperheroImage
+import com.robgar.marvel.core.ui.superheroes.SuperheroesState
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.impl.annotations.RelaxedMockK
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+
+class GetSuperheroesUseCaseTest {
+
+    @RelaxedMockK
+    private lateinit var marvelRepository: MarvelRepository
+
+    lateinit var getSuperheroesUseCase: GetSuperheroesUseCase
+
+    val superheroes = listOf<Superhero>(
+        Superhero(1, "Spiderman", "", SuperheroImage("http://www.marvel.com", "jpg"), null, null),
+        Superhero(2, "Batman", "", SuperheroImage("image_not_available", "jpg"), null, null),
+        Superhero(3, "Wolverine", "", SuperheroImage("http://www.marvel.com", "jpg"), null, null),
+        Superhero(4, "Antman", "", SuperheroImage("image_not_available", "jpg"), null, null),
+        Superhero(5, "Ironman", "", SuperheroImage("http://www.marvel.com", "jpg"), null, null),
+        Superhero(6, "Hulk", "", SuperheroImage("http://www.marvel.com", "jpg"), null, null)
+    )
+
+    @Before
+    fun onBefore() {
+        MockKAnnotations.init(this)
+        getSuperheroesUseCase = GetSuperheroesUseCase(marvelRepository)
+    }
+
+    @Test
+    fun `get superheroes returns success`() = runBlocking {
+        // Given
+        val superheroesList = superheroes
+        coEvery { marvelRepository.getSuperheroes() } returns SuperheroesState.Success(superheroesList)
+
+        // When
+        val response = getSuperheroesUseCase()
+
+        // Then
+        assert(response is SuperheroesState.Success)
+    }
+
+}
