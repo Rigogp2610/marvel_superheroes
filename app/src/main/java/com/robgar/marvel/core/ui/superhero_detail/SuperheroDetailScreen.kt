a0c032915819f1489c83284a476aee0f69671247
Rigogp2610 <rgarcia2610p@gmail.com>
2023-11-28 21:25:18 +0100

    Add Superhero detail and dialogs

--- app/src/main/java/com/robgar/marvel/core/ui/superhero_detail/SuperheroDetailScreen.kt
+++ app/src/main/java/com/robgar/marvel/core/ui/superhero_detail/SuperheroDetailScreen.kt
@@ -1,37 +1,38 @@
 package com.robgar.marvel.core.ui.superhero_detail
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.navigation.NavHostController
+import coil.compose.AsyncImage
 import com.robgar.marvel.core.data.network.model.Superhero
 import com.robgar.marvel.ui.theme.AppColor
+import com.robgar.marvel.ui.theme.BorderStrokeColor
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SuperheroDetailScreen(navController: NavHostController, superhero: Superhero?) {
-    Scaffold (
+    Scaffold(
         topBar = {
             SuperheroeTopBar(superhero) {
                 navController.navigateUp()
             }
         }
     ) {
-        Column (
-            Modifier
-                .fillMaxSize()
-                .background(Color.White)
-                .padding(it)
-        ) {
-
-        }
+        if (superhero != null) SuperheroContent(superhero = superhero, paddingValues = it)
     }
 }
 
@@ -46,8 +47,105 @@ fun SuperheroeTopBar(superhero: Superhero?, onBackScreen: () -> Unit) {
         ),
         navigationIcon = {
             IconButton(onClick = { onBackScreen() }) {
-                Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "back", tint = Color.White)
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = "back",
+                    tint = Color.White
+                )
             }
         }
     )
+}
+
+@Composable
+fun SuperheroContent(superhero: Superhero, paddingValues: PaddingValues) {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(paddingValues)
+    ) {
+        Card(
+            border = BorderStroke(1.dp, BorderStrokeColor),
+            colors = CardDefaults.cardColors(
+                containerColor = BorderStrokeColor,
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            AsyncImage(
+                model = "${superhero.thumbnail.path}.${superhero.thumbnail.extension}",
+                contentDescription = "Superhero image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.height(300.dp)
+            )
+        }
+        Text(
+            text = superhero.description,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(horizontal = 16.dp, vertical = 8.dp),
+            color = Color.Black,
+            style = TextStyle(
+                fontSize = 16.sp
+            )
+        )
+        BottomButtons(superhero = superhero)
+    }
+}
+
+@Composable
+fun BottomButtons(superhero: Superhero) {
+    ConstraintLayout(
+        Modifier
+            .fillMaxSize()
+            .padding(bottom = 16.dp)
+    ) {
+
+        val (comics, series) = createRefs()
+
+        Button(
+            modifier = Modifier
+                .width(150.dp)
+                .constrainAs(comics) {
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(series.start)
+                },
+            colors = ButtonDefaults.buttonColors(containerColor = AppColor),
+            enabled = superhero.comics?.items != null && superhero.comics.items.isNotEmpty(),
+            onClick = {  }) {
+            Text(
+                text = "Comics",
+                color = Color.White,
+                style = TextStyle(
+                    fontSize = 16.sp
+                )
+            )
+        }
+        SuperheroComicsDialog(superhero = superhero)
+        Button(
+            modifier = Modifier
+                .width(150.dp)
+                .constrainAs(series) {
+                    bottom.linkTo(parent.bottom)
+                    end.linkTo(parent.end)
+                    start.linkTo(comics.end)
+                },
+            colors = ButtonDefaults.buttonColors(containerColor = AppColor),
+            enabled = superhero.series?.items != null && superhero.series.items.isNotEmpty(),
+            onClick = {}) {
+            Text(
+                text = "Series",
+                color = Color.White,
+                style = TextStyle(
+                    fontSize = 16.sp
+                )
+            )
+        }
+        SuperheroSeriesDialog(superhero = superhero)
+
+        createHorizontalChain(comics, series, chainStyle = ChainStyle.Spread)
+    }
 }
---
+++ app/src/main/java/com/robgar/marvel/core/ui/superhero_detail/SuperheroSeriesDialog.kt
@@ -0,0 +1,67 @@
+package com.robgar.marvel.core.ui.superhero_detail
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import com.robgar.marvel.core.data.network.model.ComicSummary
+import com.robgar.marvel.core.data.network.model.SeriesSummary
+import com.robgar.marvel.core.data.network.model.Superhero
+import com.robgar.marvel.ui.theme.BorderStrokeColor
+
+@Composable
+fun SuperheroSeriesDialog(superhero: Superhero) {
+    Dialog(
+        onDismissRequest = {  }
+    ) {
+        Column(
+            Modifier
+                .background(Color.White)
+                .padding(24.dp)
+                .fillMaxWidth()
+        ) {
+            LazyColumn {
+                items(superhero.series?.items!!) {
+                    SerieItem(serie = it)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SerieItem(serie: SeriesSummary) {
+    Card(
+        border = BorderStroke(1.dp, BorderStrokeColor),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White,
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Text(
+            text = serie.name ?: "",
+            modifier = Modifier
+                .padding(8.dp),
+            color = Color.Black,
+            style = TextStyle(
+                fontSize = 16.sp
+            )
+        )
+    }
+
+}
